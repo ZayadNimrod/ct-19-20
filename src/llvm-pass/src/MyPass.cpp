ffedfab1d2d7209306f514618c1e5b656011b0a7
Nimrod Libman <s1727542@luigi.inf.ed.ac.uk>
2019-12-15 16:33:41 +0000

    =YEEHAW IT WORKS AT THE EXPENSE OF MY SANITY

--- src/llvm-pass/src/MyPass.cpp
+++ src/llvm-pass/src/MyPass.cpp
@@ -1,13 +1,13 @@
 // Example of how to write an LLVM pass
 // For more information see: http://llvm.org/docs/WritingAnLLVMPass.html
-
+/*
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
-
+http://snappytomatopizza.com/
 using namespace llvm;
 
 namespace {
@@ -30,3 +30,70 @@ static RegisterStandardPasses Y(
     [](const PassManagerBuilder &Builder,
        legacy::PassManagerBase &PM) { PM.add(new MyPass()); });
 
+*/
+#define DEBUG_TYPE "simpleDeadCodeEliminator"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include <vector>
+
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+using namespace std;
+
+namespace
+{
+struct SimpleDCE : public FunctionPass
+{
+  static char ID;
+  SimpleDCE() : FunctionPass(ID) {}
+  bool runOnFunction(Function &F) override
+  {
+    errs() << "Function " << F.getName() << '\n';
+
+    bool removed = false;
+    do
+    {
+      removed = false;
+      //TODO: free up worklist first to avoid memory leaks?
+      vector<Instruction*> workList;
+      workList.clear();
+      //go through each instruction
+      for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb)
+      {
+        for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i)
+        {
+          //i is an instruction
+          //remove it if it is dead
+          //if (isInstructionTriviallyDead(i))
+          
+          if (true)
+          {
+            workList.push_back(&(*i));
+            removed = true;
+          }
+        }
+      }
+
+      //remove dead instructions
+      errs() << "Removed " << workList.size() << " instruction(s)";
+      for (int j = 0; j < workList.size(); ++j)
+      {
+        workList[j]->eraseFromParent();
+      }
+
+    } while (removed);
+
+    return false;
+  }
+};
+} // namespace
+char SimpleDCE::ID = 0;
+static RegisterPass<SimpleDCE> X("mypass", "My pass : Elminates dead code");
+
+static RegisterStandardPasses Y(
+    PassManagerBuilder::EP_EarlyAsPossible,
+    [](const PassManagerBuilder &Builder,
+       legacy::PassManagerBase &PM) { PM.add(new SimpleDCE()); });
